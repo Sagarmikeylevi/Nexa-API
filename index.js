4aed4d20c2e458fa68372337f861b0006f1345e7
unknown <sagarmikeylevi@gmail.com>
2024-03-04 19:10:10 +0530

    use swagger for api-docs

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 import productRouter from "./src/features/product/product.routes.js";
 import userRouter from "./src/features/user/user.routes.js";
 import cartRouter from "./src/features/cart/cart.routes.js";
@@ -7,12 +8,14 @@ import bodyParser from "body-parser";
 import { config } from "dotenv";
 import jwtAuth from "./src/middlewares/jwt.middleware.js";
 config();
+import apiDocs from "./swagger.json" assert { type: "json" };
 
 const server = express();
 const PORT = process.env.PORT;
 
 server.use(bodyParser.json());
 
+server.use("/api-docs", swagger.serve, swagger.setup(apiDocs));
 server.use("/api/products", jwtAuth, productRouter);
 server.use("/api/cart", jwtAuth, cartRouter);
 server.use("/api/users", userRouter);
